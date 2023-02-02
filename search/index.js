2d7a8d35c9bbf10e12748b8391404132e0fc4513
eliasraffaelli0 <eliasraffaelli@gmail.com>
2023-02-02 17:55:29 -0300

    gets first 100 items using pageToken, need to figure out how to make a loop or something to get all items

--- search/index.js
+++ search/index.js
@@ -4,11 +4,34 @@ const { google } = require('googleapis');
 google.youtube('v3').playlistItems.list({
     key: process.env.YOUTUBE_TOKEN,
     part: 'snippet',
-    playlistId: 'PLuSl8rFJUgCDlo-kMVZr1kAjA_RT12eg9',
+    playlistId: 'PLv8g3HY3OAPgAvPY-ixoTfoTAMXb4GHl8',
     maxResults: 50,
 }).then((response)=>{
-    const {data} = response;
+    let {data} = response;
+    let page = data.nextPageToken;
     data.items.forEach(item => {
         console.log(item.snippet.title)
+        console.log(item.snippet.videoOwnerChannelTitle)
+        console.log('---------------------------------------')
     });
+
+}).catch((err)=> console.log(err));
+// pageToken: 'EAAaBlBUOkNESQ',
+
+google.youtube('v3').playlistItems.list({
+    key: process.env.YOUTUBE_TOKEN,
+    part: 'snippet',
+    pageToken: 'EAAaBlBUOkNESQ',
+    playlistId: 'PLv8g3HY3OAPgAvPY-ixoTfoTAMXb4GHl8',
+    maxResults: 50,
+}).then((response)=>{
+    let {data} = response;
+    console.log(data);
+    let page = data.nextPageToken;
+    data.items.forEach(item => {
+        console.log(item.snippet.title)
+        console.log(item.snippet.videoOwnerChannelTitle)
+        console.log('---------------------------------------')
+    });
+
 }).catch((err)=> console.log(err));
